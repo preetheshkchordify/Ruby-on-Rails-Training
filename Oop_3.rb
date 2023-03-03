958a0044cdc600b65fb393dc4dc435c70416f999
Harikrishnan R <hari.krishan@rubyians.com>
2023-03-03 22:14:07 +0530

    updated

--- Oop_3.rb
+++ Oop_3.rb
@@ -5,7 +5,35 @@
 #the inventory
 
 class Bookstore
+  def initialize
+    @inventory=[]
+  end
+end
 
-  def 
+class Book < Bookstore
+  def add_inventory(book_detail)
+    @inventory.push(book_detail)
+  end
 
+  def display_inventory
+    puts @inventory
+  end
 
+  def displayDetails
+    super
+  end
+  def total_value
+    puts "Total value is #{@inventory.map {|x| x[:price]}.sum}"
+    
+
+  end
+end
+
+book1=Book.new
+books = [{name: '1984', author: 'George Orwell', price: 1399},
+        {name: '1985', author: 'George Martin', price: 800}]
+books.each do |book|
+  book1.add_inventory(book)
+end
+book1.display_inventory
+book1.total_value
