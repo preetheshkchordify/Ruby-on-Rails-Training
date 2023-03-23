abfbd069244bed49b2effa5d07fe586b4be92bed
Harikrishnan R <hari.krishan@rubyians.com>
2023-03-23 11:20:07 +0530

    Api

--- employee/app/controllers/api/v1/employes_controller.rb
+++ employee/app/controllers/api/v1/employes_controller.rb
@@ -1,16 +1,15 @@
 class Api::V1::EmployesController < ApplicationController
-  before_action :set_employ, only: [ :show, :update, :destroy ]
+  before_action :set_employ, only: %i[show update destroy]
 
   def index
-    render json: {message: "EmployeeList", data: Employe.all.order(:id)}, status: :ok
+    render json: { message: 'EmployeeList', data: Employe.all.order(:id) }, status: :ok
   end
 
   def show
-    # employ = Employe.find_by(id: params[:id])
     if @employ
-      render json: {message: "EmployeeDetails",data: @employ}, status: :ok
+      render json: { message: 'EmployeeDetails', data: @employ }, status: :ok
     else
-      render json: {message: "Unable to find the employee"}, status: :bad_request
+      render json: { message: 'Unable to find the employee' }, status: :bad_request
     end
   end
 
@@ -18,33 +17,39 @@ class Api::V1::EmployesController < ApplicationController
     employ = Employe.new(employ_params)
     if employ
       employ.save
-    render json: {message: "New Employee Created", data: employ}, status: :created
+      if employ.errors.present?
+        render json: { message: employ.errors.full_messages }, status: :bad_request
+      else
+        render json: { message: 'New Employee Created', data: employ }, status: :created
+      end
     else
-      render json: {message: "Unable to create"}, status: :bad_request
+      render json: { message: 'Unable to create' }, status: :bad_request
     end
   end
 
   def update
-    # employ = Employe.find_by(id: params[:id])
+
     if @employ
       @employ.update(employ_params)
-      render json: {message: "Employee details updated", data: @employ}, status: :ok
+      if @employ.errors.present?
+        render json: { message: @employ.errors.full_messages }, status: :bad_request
+      else
+        render json: { message: 'Employee details updated', data: @employ }, status: :ok
+      end
     else
-      render json: {message: "Employee Details cannot be updated"}, status: :bad_request
+      render json: { message: 'Employee Details cannot be updated' }, status: :bad_request
     end
   end
 
 
   def destroy
-    # employ = Employe.find_by(id: params[:id])
     if @employ
       @employ.destroy
-      render json: {message: "Employee Deleted"}, status: :ok
+      render json: { message: 'Employee Deleted' }, status: :ok
     else
-      render json: {message: "Cannot find the employee"}, status: :not_found
+      render json: { message: 'Cannot find the employee' }, status: :not_found
     end
   end
-
 
   private
 
@@ -52,9 +57,7 @@ class Api::V1::EmployesController < ApplicationController
     @employ = Employe.find_by(id: params[:id])
   end
 
-
   def employ_params
     params.require(:employe).permit(:name, :age, :email, :dept, :location)
   end
-
 end
--- employee/app/models/employe.rb
+++ employee/app/models/employe.rb
@@ -1,4 +1,4 @@
 class Employe < ApplicationRecord
-  validates :name, :dept, :email, presence: true
+  validates :name, :dept, :location, :age, :email, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
